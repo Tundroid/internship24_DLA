a840b53ebc82a5799b3074dc6f8b9663b1e84d35
Felix Kimbu <felixkimbu1@gmail.com>
2024-07-11 13:24:58 +0100

    More C: added INT to types.c

--- types.c
+++ types.c
@@ -6,4 +6,8 @@ int main(){
     printf("MAX SHORT SIGNED: %d\n", SHRT_MAX);
     printf("MIN SHORT UNSIGNED: %d\n", 0);
     printf("MAX SHORT UNSIGNED: %d\n", USHRT_MAX);
+    printf("MIN INT SIGNED: %d\n", INT_MIN);
+    printf("MAX INT SIGNED: %d\n", INT_MAX);
+    printf("MIN INT UNSIGNED: %d\n", 0);
+    printf("MAX INT UNSIGNED: %d\n", UINT_MAX);
 }
